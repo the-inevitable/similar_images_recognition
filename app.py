2bda3ed810ada2e39f902bc9d9b0950d45946471
Vlad Khymenko <vkhymenkov@gmail.com>
2019-05-30 09:33:08 +0300

    clarified comments

--- app.py
+++ app.py
@@ -19,19 +19,13 @@ filepathes = glob.glob(args.path + '*' if args.path.endswith('/') else args.path
 DIFFERENTIATION_LIMIT = 6000
 
 def compare_all(filepaths): 
-	"""
-	A function to calculate difference between
-	two images, presented as np arrays.
-	Takes 2 image paths as parameters (string).
-	Prints a message if given images have 
-	difference within the limit.
-	How it works: 
-	- each image from the input gets greyscaled, resized (i.e. normalized)
-	and converted to an array of integers,
-	- calculate difference between both image arrays and reduce it to a summation
-	- if calculated difference is less than above defined Limit,
-	then the images are quite similar.
 	""" 
+	A function to go through all files (I hope there are images only :)) 
+	and determine similar images by comparing each image
+	to every other image in a folder in a nested loop, which gives us O(n^2) per se. 
+	Takes a sequence of paths to each image as a parameter. 
+	Calls compare_two function on each subiteration if filenames are not identical.
+	"""
 	tstart_compare = time.time() 
 	for filepath in filepaths: 
 		for filepath_to_compare in filepathes: 
@@ -46,7 +40,14 @@ def compare_two(path1, path2):
 	A function to calculate difference between
 	two images, presented as np arrays.
 	Takes 2 image paths as parameters (string).
-	Prints a message if given images are differenct enough.
+	Prints a message if given images have 
+	difference within the limit.
+	How it works: 
+	- each image from the input gets greyscaled, resized (i.e. normalized)
+	and converted to an array of integers,
+	- calculate difference between both image arrays and reduce it to a summation
+	- if calculated difference is less than above defined Limit,
+	then the images are quite similar.
 	""" 
 	images_container = [np.array(Image.open(f).convert('L').resize((16,16), resample=Image.BICUBIC)).astype(np.int) for f in [path1,path2]]
 	difference = np.abs(images_container[0] - images_container[1]).sum()
