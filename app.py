25f4e54213cacde0b1014b05c8eaa3cc90897171
Vlad Khymenko <vkhymenkov@gmail.com>
2019-05-30 09:28:50 +0300

    clarified comment

--- app.py
+++ app.py
@@ -19,13 +19,19 @@ filepathes = glob.glob(args.path + '*' if args.path.endswith('/') else args.path
 DIFFERENTIATION_LIMIT = 6000
 
 def compare_all(filepaths): 
-	""" 
-	A function to go through all files (I hope there are images only :)) 
-	and determine similar images by comparing each image
-	to every other image in a folder in a nested loop, which gives us O(n^2) per se. 
-	Takes a sequence of paths to each image as a parameter. 
-	Calls compare_two function on each subiteration if filenames are not identical.
 	"""
+	A function to calculate difference between
+	two images, presented as np arrays.
+	Takes 2 image paths as parameters (string).
+	Prints a message if given images have 
+	difference within the limit.
+	How it works: 
+	- each image from the input gets greyscaled, resized (i.e. normalized)
+	and converted to an array of integers,
+	- calculate difference between both image arrays and reduce it to a summation
+	- if calculated difference is less than above defined Limit,
+	then the images are quite similar.
+	""" 
 	tstart_compare = time.time() 
 	for filepath in filepaths: 
 		for filepath_to_compare in filepathes: 
